448735b30a06eb7848d96bd3de3ad0de8eddd5e3
Orlin_ <orlin221@abv.bg>
2023-11-23 20:54:55 +0200

    Login Page
    I created new class in the ViewModels - LoginViewModel, using validation annotations. Created AccountController, with 2 Login methods, one get, the other post. In constructor, I get the information about userManager, signInManager and the context. Firstly in the POST method there is a validation and after that, I get the user email. Then, I check if the user is not null and after that, I check if the password is the same as it should be to sign in. if one of this is not true, it returns view Error TempData with nice error text. New Login view added, bootstrap form and validation.

---
+++ RaceRunApp/RaceRunApp/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RaceRunApp.Data;
+using RaceRunApp.Models;
+using RaceRunApp.ViewModels;
+
+namespace RaceRunApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded) 
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                TempData["Error"] = "Wrong credentials, Please, try again";
+                return View(loginViewModel);
+            }
+            TempData["Error"] = "Wrong credentials, Please try again";
+            return View(loginViewModel);
+        }
+    }
+}
