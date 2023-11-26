5db1e4a757a890093ea6cdfe8111ac476a7ac720
Orlin_ <orlin221@abv.bg>
2023-11-26 14:58:47 +0200

    Details Page - Displaying
    New controller added - "UserController".
    New interface added - "IUserRepository"
    2 Asynchronous methods - GetAllUsers,GetUserById. In the layout displaying the "UserController" with action "Index" only if you are logged in. In the Repository - GetAllUsers - returns a list of users.  GetUserById - returns a user with the id that is searched.

---
+++ RaceRunApp/RaceRunApp/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RaceRunApp.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet("users")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
