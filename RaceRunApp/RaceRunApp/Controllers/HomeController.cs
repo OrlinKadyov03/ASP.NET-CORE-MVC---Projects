a915eb7afbb0567a3a1fd3330c868aa0a60694c9
Orlin_ <orlin221@abv.bg>
2023-12-01 15:51:49 +0200

    IpInfo.. Real city and state
    Got the Ip Address from IPInfo.io
    New ViewModel added for the Ip Configuration. Little improvements in the Index View of HomeController. In HomeController, In Try Catch I set the settings for right configuration for the Ip. One If check and at the end, I return the ViewModel. To make this work on you, you have to sign up in IpInfo.io to get your IpAddress.

--- RaceRunApp/RaceRunApp/Controllers/HomeController.cs
+++ RaceRunApp/RaceRunApp/Controllers/HomeController.cs
@@ -1,21 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RaceRunApp.Helpers;
+using RaceRunApp.Interfaces;
 using RaceRunApp.Models;
+using RaceRunApp.ViewModels;
 using System.Diagnostics;
+using System.Net;
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace RaceRunApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IClubRepository clubRepository)
         {
-            _logger = logger;
+            this._logger = logger;
+            this._clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IpInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IpInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if(homeViewModel.City != null) 
+                {
+                    homeViewModel.Clubs = await _clubRepository.GetClubByCity(homeViewModel.City);
+                }
+                else 
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception ex) 
+            {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ RaceRunApp/RaceRunApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RaceRunApp.Models;
+
+namespace RaceRunApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club> Clubs { get; set; }
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+
+    }
+}
