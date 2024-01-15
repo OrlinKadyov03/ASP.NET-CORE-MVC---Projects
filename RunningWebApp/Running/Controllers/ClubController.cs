001604da882faac4908b826427f199007d4f887e
Orlin_ <orlin221@abv.bg>
2024-01-15 16:15:39 +0200

    Layout, Getting the real data
    Getting the real data from the database. Index  method returns all the clubs in ClubController and Index method in RaceController returns all the races. In Club Views, using foreach, I get all the data from the database like image, title and description, plus good design using bootstrap. The same stays for Race Views.

--- RunningWebApp/Running/Controllers/ClubController.cs
+++ RunningWebApp/Running/Controllers/ClubController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Running.Data;
 
 namespace Running.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
--- RunningWebApp/Running/Controllers/RaceController.cs
+++ RunningWebApp/Running/Controllers/RaceController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Running.Data;
 
 namespace Running.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
