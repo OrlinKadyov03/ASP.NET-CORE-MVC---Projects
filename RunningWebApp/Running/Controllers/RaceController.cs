10fb8eda0986f711d2cfa9f6142189f01e1719a7
Orlin_ <orlin221@abv.bg>
2024-01-21 20:48:21 +0200

    PhotoUpload added
    Using cloudinary, needs some more work and then you can upload images that you want!

--- RunningWebApp/Running/Controllers/RaceController.cs
+++ RunningWebApp/Running/Controllers/RaceController.cs
@@ -25,5 +25,20 @@ namespace Running.Controllers
             Race race = await _raceRepository.GetIdByAsync(id);
             return View(race);
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Race race) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(race);
+            }
+            _raceRepository.Add(race);
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ RunningWebApp/Running/Services/PhotoService.cs
@@ -0,0 +1,31 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+using Running.Helpers;
+using Running.Interfaces;
+
+namespace Running.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoService(IOptions<CloudinarySettings> config)
+        {
+            var acc = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+              );
+            _cloudinary = new Cloudinary(acc);
+        }
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+           var uploadResult = new ImageUploadResult();
+        }
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
