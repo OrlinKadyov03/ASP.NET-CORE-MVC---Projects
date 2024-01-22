077a16b0e360d4eeec56900eff9ba95fc3f007bc
Orlin_ <orlin221@abv.bg>
2024-01-22 09:22:28 +0200

    PhotoService almost done
    AddPhoto method is done, one small check if there is a file, after that some image settings and at the end upload the photo to cloudinary.

--- RunningWebApp/Running/Services/PhotoService.cs
+++ RunningWebApp/Running/Services/PhotoService.cs
@@ -1,5 +1,6 @@
 ﻿using CloudinaryDotNet;
 using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using Microsoft.Extensions.Options;
 using Running.Helpers;
 using Running.Interfaces;
@@ -18,9 +19,21 @@ namespace Running.Services
               );
             _cloudinary = new Cloudinary(acc);
         }
-        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
         {
            var uploadResult = new ImageUploadResult();
+
+            if ( file.Length > 0) 
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face")
+                };
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+            return uploadResult;
         }
 
         public Task<DeletionResult> DeletePhotoAsync(string publicid)
