4597699d133d831830cecedcf25a9fd6437d6fd2
Orlin_ <orlin221@abv.bg>
2024-01-25 17:53:20 +0200

    Register & Logout added
    New RegisterViewModel creating, 3 properties inside with data validation. In the layout, I added link to the register action. New View - Register, with form inside. In the AppUser class, the AddressId, i made it nullable. In AccountController, I check if the ema9l address is already used or not.  If it's used an error comes, else  the user is created, with role User and is redirected to the races.

--- RunningWebApp/Running/Models/AppUser.cs
+++ RunningWebApp/Running/Models/AppUser.cs
@@ -12,7 +12,7 @@ namespace Running.Models
         public int? Mileage { get; set; }
         [ForeignKey("Address")]
 
-        public int AddressId { get; set; }
+        public int? AddressId { get; set; }
 
         public Address? Address { get; set; }
 
