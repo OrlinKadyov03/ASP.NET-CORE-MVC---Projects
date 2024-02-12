c5cb7cc8c24dccc741f654405f1d0b060aca6b95
Orlin_ <orlin221@abv.bg>
2024-02-12 16:50:04 +0200

    Ip Location
    Now the application, have the access to your location , state and city. Which means in the first page, when you are in the application, you can see running clubs near your city. For that purpose you need to have registration in ipinfo to get your current ip url. You need to paste it in HomeController, where the string url is equals to "". Small modifications in Index View file.

--- RunningWebApp/Running/Controllers/HomeController.cs
+++ RunningWebApp/Running/Controllers/HomeController.cs
@@ -1,21 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Newtonsoft.Json;
+using Running.Helpers;
+using Running.Interfaces;
 using Running.Models;
+using Running.ViewModels;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace Running.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IClubRepository clubRepository)
         {
-            _logger = logger;
+            this._logger = logger;
+            this._clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if (homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await _clubRepository.GetAllClubByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception)
+            {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ RunningWebApp/Running/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Running.Models;
+
+namespace Running.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club> Clubs { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+    }
+}
