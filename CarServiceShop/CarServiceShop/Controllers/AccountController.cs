10bba3eb5481a1671c98f2009b6221683aad0dab
Orlin_ <orlin221@abv.bg>
2024-02-28 19:33:01 +0200

    Login,Register and Logout
    Created Login method, with the help of userManager and singInManager, I completed the method, three if checks, the first one if the user exists, the second if the user passwords doesn't match and the third if the user successfully logged in. In Register method, I check again if the user exists  and if exists, an error message comes. Then, I bind the Email and Username , to the EmailAddress of the RegisterViewModel, in the end, I create new user. The logout is the most simple one.  Changes in the views too, now became so much better application.

--- CarServiceShop/CarServiceShop/Controllers/AccountController.cs
+++ CarServiceShop/CarServiceShop/Controllers/AccountController.cs
@@ -23,5 +23,63 @@ namespace CarServiceShop.Controllers
             var response = new LoginViewModel();
             return View(response);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Car");
+                    }
+                }
+                TempData["Error"] = "Wrong credentials. Please,try again";
+                return View(loginViewModel);
+            }
+            TempData["Error"] = "Wrong credentials. Please,try again";
+            return View(loginViewModel);
+        }
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel) 
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+            if (user != null) 
+            {
+                TempData["Error"] = "This email address is already in user";
+                return View(registerViewModel);
+            }
+
+            var newUser = new Owner()
+            {
+                Email = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            return RedirectToAction("Index", "Car");
+        }
+        public async Task<IActionResult> Logout() 
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Car");
+        }
     }
 }
